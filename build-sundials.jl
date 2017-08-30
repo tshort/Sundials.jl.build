f94b8d99f9433be2d3203c71e32caa73787123b9
Tom Short <tshort.rlists@gmail.com>
2017-08-30 16:45:52 -0400

    Update for BinDeps2 changes

--- build-sundials.jl
+++ build-sundials.jl
@@ -1,9 +1,17 @@
 #!/usr/bin/env julia
 using BinDeps2
+using SHA
 
 basedir = pwd()
 
-BinDeps2.temp_prefix() do prefix
+# Our build products will go into ./products
+out_path = joinpath(pwd(), "products")
+rm(out_path; force=true, recursive=true)
+mkpath(out_path)
+
+products = Dict()
+
+temp_prefix() do prefix
     src_path = joinpath(prefix, "src")
     build_path = joinpath(prefix, "build")
     try mkpath(src_path) end
@@ -13,32 +21,40 @@ BinDeps2.temp_prefix() do prefix
     src_url = "https://computation.llnl.gov/projects/sundials/download/sundials-2.7.0.tar.gz"
     src_hash = "d39fcac7175d701398e4eb209f7e92a5b30a78358d4a0c0fcc23db23c11ba104"
     
-    BinDeps2.download_verify_unpack(src_url, src_hash, src_path; verbose=true)
-    
+    download_verify_unpack(src_url, src_hash, src_path; verbose=true)
+
     # Build for many platforms
-    for platform in BinDeps2.supported_platforms()
+    for platform in supported_platforms()
         cd(build_path) do
             mkdir(string(platform))
-	        cd(string(platform)) do 
-                target = BinDeps2.platform_map(platform)
-                libsundials = BinDeps2.LibraryResult(joinpath(BinDeps2.libdir(prefix), target, "libsundials"))
+	    cd(string(platform)) do 
+                target = platform_triplet(platform)
+                libsundials = LibraryResult(joinpath(libdir(prefix), target, "libsundials"))
                 extras = platform == :win64 ? `-DCMAKE_SYSTEM_NAME=Windows` : ``
                 cmake_args = `-DCMAKE_INSTALL_PREFIX=/inst/$platform -DEXAMPLES_ENABLE=OFF $extras`
                 steps = [`cmake $cmake_args ../../src/sundials-2.7.0/`, 
                          `make -j4`, 
                          `make install`]
-                dep = BinDeps2.Dependency("sundials", [libsundials], steps, platform, prefix)
-                BinDeps2.build(dep; verbose=true, force=true)
+                dep = Dependency("sundials", [libsundials], steps, platform, prefix)
+                build(dep; verbose=true, force=true)
+                rm("$basedir/libsundials_$(target).tar.gz"; force=true)
+                tarball_path = package(BinDeps2.Prefix("$(prefix.path)/inst/$platform"), 
+			               "$basedir/products/libsundials", 
+                                       platform=platform, verbose=true)
+                # Once we're built up, go ahead and package this prefix out
+                tarball_hash = open(tarball_path, "r") do f
+                    return bytes2hex(sha256(f))
+                end
+                products[target] = (basename(tarball_path), tarball_hash)
+                info("Built and saved at $(tarball_path)")
             end
         end
     end
-    # Next, package it up as a .tar.gz file
-    for platform in BinDeps2.supported_platforms()
-        # This could go in the loop above, but it's nice to see them all at the end.
-        rm("$basedir/libsundials_$(platform).tar.gz"; force=true)
-        tarball_path = BinDeps2.package(BinDeps2.Prefix("$(prefix.path)/inst/$platform"), "$basedir/libsundials", 
-                                                        platform=platform, verbose=true)
-        info("Built and saved at $(tarball_path)")
-    end
 end
- 
+
+# In the end, dump an informative message telling the user how to download/install these
+info("Hash/filename pairings:")
+for target in keys(products)
+    filename, hash = products[target]
+    println("    \"$(target)\" => (\"\$prefix/$(filename)\", \"$(hash)\"),")
+end 
